8f0133b2443164912361457e2bf1239702748083
Igor Kozlowski <igor3648@gmail.com>
2023-02-14 18:10:56 +0100

    <Header /> changed to <h1/2>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Form from "./Form";
-import Header from './Header';
 import Section from "./Section";
 
 function App() {
   return (
     <>
-      <Header
-        title="Kalkulator walut"
-      />
+      <h1 className="header">
+        Kalkulator walut
+      </h1>
       <Form />
+      <h2 className="header secondHeader">
+        Pola wymagane do wypełnienia oznaczone są *
+      </h2>
       <Section
         title="Uśrednione kursy wymiany z dnia 09.09.2022"
       />
