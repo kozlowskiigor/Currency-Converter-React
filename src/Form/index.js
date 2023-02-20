a8e4488ffac885fcc00c4b02857830e8a03f7a96
Igor Kozlowski <igor3648@gmail.com>
2023-02-20 18:44:15 +0100

    incomplete adding interactions

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,47 +1,89 @@
+import React, { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <fieldset className="form__fieldset">
-      <legend className="form__legend">Kalkulator walut</legend>
-      <p className="form__line">
-        <label>
-          <span className="form__labelText">Kwota*</span>
-          <input type="number" min="1" step="any" placeholder="Wpisz kwotę" required className="form__field" />
-        </label>
-      </p>
-      <p className="form__line">
-        <label>
-          <span className="form__labelText">Przewalutowanie z</span>
-          <select className="form__field">
-            <option value="PLN">PLN - polski złoty</option>
-            <option value="GBP">GBP - brytyjski funt szterling</option>
-            <option value="USD">USD - dolar amerykański</option>
-            <option value="CZK">CZK - korona czeska</option>
-            <option value="EUR">EUR - euro</option>
-          </select>
-        </label>
-      </p>
-      <p className="form__line">
-        <label>
-          <span className="form__labelText">Przewalutowanie na</span>
-          <select className="form__field">
-            <option value="PLN">PLN - polski złoty</option>
-            <option value="GBP">GBP - brytyjski funt szterling</option>
-            <option value="USD">USD - dolar amerykański</option>
-            <option value="CZK">CZK - korona czeska</option>
-            <option value="EUR">EUR - euro</option>
-          </select>
-        </label>
-      </p>
-      <button className="form__button">Przewalutuj</button>
-      <span className="form__labelText form__resault">
-        <p className="convertedAmount">
-          Przewalutowana kwota<strong className="resault__value"></strong>
+const Form = ({ calculateResult }) => {
+  const [newValue, setNewValue] = useState("");
+  const [inputCurrency, setInputCurrency] = useState("PLN");
+  const [outputCurrency, setOutputCurrency] = useState("PLN")
+
+  const onInputChange = ({ target }) => setNewValue(target.value)
+  const onInputSeclectChange = ({ target }) => setInputCurrency(target.value)
+  const onOutputSeclectChange = ({ target }) => setOutputCurrency(target.value)
+
+  const result = calculateResult(inputCurrency, outputCurrency, newValue);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult();
+    setNewValue("");
+  }
+
+  return (
+    <form
+      className="form"
+      onSubmit={onFormSubmit}
+    >
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Kalkulator walut</legend>
+        <p className="form__line">
+          <label>
+            <span className="form__labelText">Kwota*</span>
+            <input
+              required
+              min="1"
+              step="any"
+              type="number"
+              className="form__field"
+              placeholder="Wpisz kwotę"
+              value={newValue}
+              onChange={onInputChange}
+            />
+          </label>
         </p>
-      </span>
-    </fieldset>
-  </form>
-);
+        <p className="form__line">
+          <label>
+            <span className="form__labelText">Przewalutowanie z</span>
+            <select
+              className="form__field"
+              value={inputCurrency}
+              onChange={onInputSeclectChange}
+            >
+              <option value="PLN">PLN - polski złoty</option>
+              <option value="GBP">GBP - brytyjski funt szterling</option>
+              <option value="USD">USD - dolar amerykański</option>
+              <option value="CZK">CZK - korona czeska</option>
+              <option value="EUR">EUR - euro</option>
+            </select>
+          </label>
+        </p>
+        <p className="form__line">
+          <label>
+            <span className="form__labelText">Przewalutowanie na</span>
+            <select
+              className="form__field"
+              value={outputCurrency}
+              onChange={onOutputSeclectChange}
+            >
+              <option value="PLN">PLN - polski złoty</option>
+              <option value="GBP">GBP - brytyjski funt szterling</option>
+              <option value="USD">USD - dolar amerykański</option>
+              <option value="CZK">CZK - korona czeska</option>
+              <option value="EUR">EUR - euro</option>
+            </select>
+          </label>
+        </p>
+        <button className="form__button">Przewalutuj</button>
+        <span className="form__labelText form__resault">
+          <p className="convertedAmount">
+            Przewalutowana kwota
+            <strong className="resault__value">
+              {result.toFixed(2)} {outputCurrency}
+            </strong>
+          </p>
+        </span>
+      </fieldset>
+    </form>
+  );
+};
 
 export default Form;
