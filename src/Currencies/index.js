25911feb31ced5cc0a4da097c39a57c491df1b94
Igor Kozlowski <igor3648@gmail.com>
2023-02-23 22:37:17 +0100

    currencies added

---
+++ src/Currencies/index.js
@@ -0,0 +1,29 @@
+// const currencies = [
+//   pln = {
+//     short: "PLN",
+//     value: 1,
+//     name: "Polski złoty",
+//   },
+//   gbp = {
+//     short: "GBP",
+//     value: 5.43913,
+//     name: "Polski złoty",
+//   },
+//   usd = {
+//     short: "USD",
+//     value: 4.68485,
+//     name: "Dolar amerykański",
+//   },
+//   czk = {
+//     short: "CZK",
+//     value: 0.19224,
+//     name: "Korona czeska",
+//   },
+//   eur = {
+//     short: "EUR",
+//     value: 4.71790,
+//     name: "Euro",
+//   },
+// ];
+
+// export default currencies;
