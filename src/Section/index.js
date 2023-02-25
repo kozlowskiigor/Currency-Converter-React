c8c254c367480c74874c53fdb7ea045ce64faef6
Igor Kozlowski <igor3648@gmail.com>
2023-02-25 14:20:24 +0100

    map used in section

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,15 +2,21 @@ import "./style.css";
 import currencies from "../Currencies";
 
 const Section = ({ title }) => {
+  const averageExchangeRates = currencies.map((element, i) => {
 
-  const averageExchangeRates = currencies.map((element) => {
-    if (element.short !== "PLN")
+    if (element.short === "PLN")
+      return "";
+
     return (
-      <p className="averageExchangeRates__item">
+      <p
+        key={i}
+        className="averageExchangeRates__item"
+      >
         1,00 {element.short} = {element.value} PLN
       </p>
-    )
-  })
+    );
+  });
+
   return (
     <>
       <section className="averageExchangeRates">
@@ -18,33 +24,11 @@ const Section = ({ title }) => {
           <header className="averageExchangeRates__item">
             {title}
           </header>
-          <p className="averageExchangeRates__item">
-            {averageExchangeRates}
-          </p>
+          {averageExchangeRates}
         </div>
       </section>
     </>
-  )
-}
-
-// <section className="averageExchangeRates">
-//   <div className="averageExchangeRates--InsideMargin">
-//     <header className="averageExchangeRates__item">
-//       {title}
-//     </header>
-//     <p className="averageExchangeRates__item">
-//       1,00 EUR = 4,71790 PLN
-//     </p>
-//     <p className="averageExchangeRates__item">
-//       1,00 GBP = 5,43913 PLN
-//     </p>
-//     <p className="averageExchangeRates__item">
-//       1,00 USD = 4,68485 PLN
-//     </p>
-//     <p className="averageExchangeRates__item">
-//       1,00 CZK = 0,19224 PLN
-//     </p>
-//   </div>
-// </section>
+  );
+};
 
 export default Section;
