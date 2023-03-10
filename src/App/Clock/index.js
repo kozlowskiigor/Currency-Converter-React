0b59d0032b2f399402e960780b8bb1be865016da
Igor <igor3648@gmail.com>
2023-03-10 11:57:13 +0100

    code formatted

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -20,11 +20,11 @@ export const Clock = () => {
             {" "}
             {date.toLocaleString(undefined, {
                 weekday: "long",
+                day: "numeric",
+                month: "long",
                 hour: "2-digit",
                 minute: "2-digit",
                 second: "2-digit",
-                day: "numeric",
-                month: "long",
             })}
         </div>
     )
