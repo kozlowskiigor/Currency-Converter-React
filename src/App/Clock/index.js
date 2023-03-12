b164117646267b2c957524c07175d9cfd562dc0f
Igor Kozlowski <igor3648@gmail.com>
2023-03-12 14:49:22 +0100

    code formatted

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -2,32 +2,32 @@ import { useState, useEffect } from 'react';
 import "./style.css";
 
 const formatDate = (date) => date.toLocaleString(undefined, {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
 });
 
 export const Clock = () => {
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
 
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-    return (
-        <div className="clock">
-            Dzisiaj jest
-            {" "}
-            {formatDate(date)}
-        </div>
-    );
+  return (
+    <div className="clock">
+      Dzisiaj jest
+      {" "}
+      {formatDate(date)}
+    </div>
+  );
 };
