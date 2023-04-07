fe2505abfa8b97feb83d320ee7020cef634a019e
Igor <igor3648@gmail.com>
2023-04-07 18:07:36 +0200

    theme created and added

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  color: {
+    transparencyBlack: "#000000d9",
+    transparencyGray: "#b3b3b3f2",
+    gray: "#b3b3b3",
+    lightGray: "#cccccc",
+    transparencyLightGray: "#e6e6e6bf",
+    teal: "#008080",
+    alto: "#d9d9d9",
+    mercury: "#e6e6e6",
+    silver: "#cccccc",
+    beige: "#f5f5dc",
+    boulder: "#777777",
+  },
+  breakpoint: {
+    mobileMax: "767",
+  },
+};
